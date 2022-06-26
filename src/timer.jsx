59f1b41027ae7c5798b90220e0d10b96c8852e6d
PitEG <enriqueiigan@gmail.com>
2022-06-26 09:06:18 -0700

    trying to fix mobile kyeboard

--- src/timer.jsx
+++ src/timer.jsx
@@ -7,7 +7,8 @@ function TimeDisplay(props) {
   const mins = parseInt(seconds/60) % 60;
   const secs = seconds % 60;
   return (
-    <div>
+      // <input type="tel" pattern="[0-9]"></input> // for mobile keyboard
+    <div tabIndex={1} onKeyPress={props.onKeyPress}>
       <p> {hrs >= 10 ? hrs : "0" + hrs} </p>
       <p> {mins >= 10 ? mins : "0" + mins} </p>
       <p> {secs >= 10 ? secs : "0" + secs} </p>
@@ -23,11 +24,12 @@ class Timer extends React.Component {
     this.state = {startAmount: startAmount, timeLeft: startAmount, ticking: false}; 
     this.handleStart = this.handleStart.bind(this);
     this.handleStop = this.handleStop.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
   tick() {
     // if current timeleft is 1 before ticking, we assume it's going to 0 and end timer
-    if (this.state.timeLeft == 1) {
+    if (this.state.timeLeft === 1) {
       clearInterval(this.intervalId);
       this.setState(()=>({ticking: false}));
     }
@@ -47,12 +49,19 @@ class Timer extends React.Component {
     this.setState({ticking:false});
   }
 
+  handleInput(e) {
+    // 0 to 9
+    if (48 <= e.charCode && e.charCode <= 57) {
+    }
+    console.log(e.key);
+  }
+
   render() {
     const ticking = this.state.ticking;
     return (
       <div>
         <h2> TIMER NAME </h2>
-        <TimeDisplay seconds={this.state.startAmount}/>
+        <TimeDisplay seconds={this.state.startAmount} onKeyPress={(e)=>{this.handleInput(e)}}/>
         <TimeDisplay seconds={this.state.timeLeft}/>
         <button onClick={ticking ? this.handleStop: this.handleStart}> 
           {ticking ? "stop" : "start" }
